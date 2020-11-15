21eedc8cb9c7dfbee8a55de94c723a6274754f78
Estefania Caceres <stepheniecaz@gmail.com>
2020-11-14 18:15:06 -0600

    template de traer datos

--- project/application/index.py
+++ project/application/index.py
@@ -1,7 +1,13 @@
 from flask import Flask,render_template,request
+import informacion.Datos as dts
 
 app = Flask(__name__)
 
 @app.route('/')
 def index(name=None):
     return render_template('index.html',name=name)
+
+@app.route('/prueba')
+def prueba():
+    z = dts.prueba()
+    return { "vals_prueba" : z }
---
+++ project/application/informacion/Datos.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import json
+
+def prueba():
+    z = pd.Series([1,2,4,np.nan,7,9])
+    print(z)
+    return 1
