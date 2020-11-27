d94adc8c4c0e16263a8d2a067635baa8846fb3e2
Estefania Caceres <stepheniecaz@gmail.com>
2020-11-27 08:53:08 -0600

    datetime index de csv

--- project/application/informacion/Prediccion.py
+++ project/application/informacion/Prediccion.py
@@ -6,6 +6,8 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 from pathlib import Path
 from pylab import rcParams
+
+import datetime
 plt.style.use('fivethirtyeight')
 
 #MAYO
@@ -19,35 +21,39 @@ df = pd.read_csv(filePath)
 mes = '05'
 pathAbril = "{}_2020_{}.csv".format(mes,contaminante)
 filePathAbril = thisPath/'datos'/pathAbril
-dfabril = pd.read_csv( filePathAbril )
+#f = lambda s: datetime.datetime.strptime(s,'%Y-%m-%d')
+dfabril = pd.read_csv( filePathAbril, parse_dates=True, index_col=0)
 print(dfabril)
+print(dfabril.index)
 
-nivelesMayo = df.loc[0:,'0':'23']
+""" nivelesMayo = df.loc[0:,'0':'23']
 nivelesAbril = dfabril
+
 print( "niveles abril" )
-print( nivelesAbril['fecha'].min() )
+#print( nivelesAbril['fecha'].min() )
 
 nivelesAbril = nivelesAbril.sort_values('fecha')
 
-print( nivelesAbril.isnull().sum() )
+#print( nivelesAbril.isnull().sum() )
 
 nivelesAbril = nivelesAbril.groupby('fecha').sum().reset_index()
 
 nivelesAbril = nivelesAbril.set_index('fecha')
+
 print(nivelesAbril.index)
-print(nivelesAbril)
+#print(nivelesAbril)
 
 y = nivelesAbril.mean()
 print(y)
 y.plot(figsize=(15,6))
-plt.show()
+#plt.show()
 
 rcParams['figure.figsize'] = 18,8
 
 decomposition = sm.tsa.seasonal_decompose(y,model='additive')
 fig = decomposition.plot()
 plt.show()
-""" promedios = []
+ promedios = []
 print(len(nivelesMayo) - 1)
 for j in range(0,len(nivelesMayo)):     #promedio de contaminante por dia
     promedios.append( nivelesMayo.iloc[j].mean() )
@@ -59,6 +65,6 @@ pror = np.array(promedios)
 print(pror)
 fig,ax = plt.subplots()
 ax.plot( np.arange(0,31), pror )
-plt.show() """
+plt.show()
 
-print("index : {}" .format(nivelesMayo.index) )
+print("index : {}" .format(nivelesMayo.index) ) """
