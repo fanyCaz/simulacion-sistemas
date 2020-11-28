eec9b4e9b31f0a33732b0cc2bad2a90963497a9d
Estefania Caceres <stepheniecaz@gmail.com>
2020-11-27 19:45:40 -0600

    datos de prediccion

--- project/application/informacion/Prediccion.py
+++ project/application/informacion/Prediccion.py
@@ -23,33 +23,14 @@ pathAbril = "{}_2020_{}.csv".format(mes,contaminante)
 filePathAbril = thisPath/'datos'/pathAbril
 
 dfabril = pd.read_csv( filePathAbril, parse_dates=True, index_col=0)
-#print(dfabril)
-#print(dfabril.index)
 
-#nivelesMayo = df.loc[0:,'0':'23']
 nivelesAbril = dfabril
 
-#print( "niveles abril" )
-#print( nivelesAbril.index )
-
 nivelesAbril = nivelesAbril.sort_values('fecha')
-
-#print( nivelesAbril.isnull().sum() )
-
-#nivelesAbril = nivelesAbril.groupby('fecha').sum().reset_index()
-
-#nivelesAbril = nivelesAbril.set_index('fecha')
-
-#print(nivelesAbril.index)
-#print(nivelesAbril)
 
 y = nivelesAbril.mean()
 
 y = y.fillna(y.bfill())
-#print(y['2020-05':])
-#print(y)
-#y.plot(figsize=(15,6))
-#plt.show()
 
 #POR PERIODO DE 12 HORAS, ENTONCES SE HACEN UN 'SEASON' DE UN DÍA A LA MITAD
 rcParams['figure.figsize'] = 18,8
@@ -92,32 +73,26 @@ print(results.summary().tables[1])
 pred = results.get_prediction(dynamic=False)
 pred_ci = pred.conf_int()
 
-ax = y['0':].plot(label='observed')
+ax = y['6':].plot(label='observed')
 pred.predicted_mean.plot(ax=ax, label='prediccion',alpha=.7)
-
+print("Predichos")
+#Datos de prediccion pred_ci
+#print(pred)
+print(pred_ci)
 ax.fill_between(pred_ci.index, pred_ci.iloc[:,0],pred_ci.iloc[:,1], color='k',alpha=.2)
 
 ax.set_xlabel('Ho9ur')
 ax.set_ylabel('Niveles PM10 i thing kfja')
 plt.legend()
-plt.show()
+#plt.show()
 print(ax)
-print(y)
+#print(y)
 
-plt.show()
+y_forecasted = pred.predicted_mean
+y_truth = y['0':]
 
-""" promedios = []
-print(len(nivelesMayo) - 1)
-for j in range(0,len(nivelesMayo)):     #promedio de contaminante por dia
-    promedios.append( nivelesMayo.iloc[j].mean() )
-    
-for j in range(0,len(nivelesAbril)):
-    promedios.append( nivelesAbril.iloc[j].mean() )
+mse = ((y_forecasted - y_truth) ** 2).mean()
+print("Validacion de error : {}".format( round(mse,2) ) )
 
-pror = np.array(promedios)
-print(pror)
-fig,ax = plt.subplots()
-ax.plot( np.arange(0,31), pror )
-plt.show()
-
-print("index : {}" .format(nivelesMayo.index) ) """
+pred_uc = results.get_forecast(steps=500)
+pred_ci = pred_uc.conf_int()
