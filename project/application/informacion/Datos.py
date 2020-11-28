75fd479a886d195a9cda8e1fbbfc2634e863505a
Estefania Caceres <stepheniecaz@gmail.com>
2020-11-27 20:34:00 -0600

    trae promedios

--- project/application/informacion/Datos.py
+++ project/application/informacion/Datos.py
@@ -4,14 +4,25 @@ import json
 import csv
 from pathlib import Path
 
-def ValoresDeMes(mes: int, contaminante: str):
+def ValoresDeMes(mes: str, contaminante: str):
     path = "{}_2020_{}.csv".format(mes,contaminante)
     thisPath = Path(__file__).parent.absolute()
     filePath = thisPath/'datos'/path
-    datos = pd.read_csv(filePath)
+    datos = pd.read_csv(filePath,parse_dates=True, index_col=0)
+    datosParaFechas = pd.read_csv(filePath)
     dFDatos = pd.DataFrame(datos)
-    fechas = dFDatos['fecha']
-    niveles = dFDatos.loc[0:,'0':'23']
+    fechas = pd.DataFrame(datosParaFechas)['fecha']
+    
+    nivelesAbril = dFDatos
+    nivelesAbril = nivelesAbril.sort_values('fecha')
+    y = nivelesAbril.mean()
+    y = y.fillna(y.bfill())
+    #print(y.to_json())
+    print(fechas)
+    
+    niveles = y
+    print(niveles)
+    #niveles = dFDatos.loc[0:,'0':'23']
     response = [
         {
         'fechas'  : fechas.to_json()
@@ -23,3 +34,5 @@ def ValoresDeMes(mes: int, contaminante: str):
     """ print( fechas )
     print( response ) """
     return response
+
+#ValoresDeMes('05','PM10')
