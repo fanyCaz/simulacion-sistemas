694330f7278036741c3dce30e05060a5181931ae
Estefania Caceres <stepheniecaz@gmail.com>
2020-11-30 11:09:32 -0600

    botones contaminante

--- project/application/static/javascript/index.js
+++ project/application/static/javascript/index.js
@@ -1,56 +1,61 @@
 
-let imprimirGrafica = (contaminante,fechas,niveles,hora_max) =>{
-    GRAFICA = document.getElementById('contaminante-'+contaminante);
-    let horaMax = document.getElementById('hora-cont-'+contaminante);
-    horaMax.innerHTML = "La hora con mayor nivel de contaminante es " + hora_max;
-    //MOSTRAR DIV
-    document.getElementById( "contaminante-info" ).style.display="block";
-    let contenedorInfo = document.getElementById("cont-" + contaminante);
-    contenedorInfo.style.display = "block";
-    let diaGrafica = document.getElementById('dia-contaminante-'+contaminante);
-    //FECHAS
-    let dates = [];
-    var x = 0;
-    while(fechas[x]){
-        dates.push( fechas[x] );
-        x++;
+var info = (contaminante) => {
+    switch(contaminante){
+        case 'PM10' : 
+            return 'info PM10';
+        case 'CO' : 
+            return 'info CO';
+        case 'Ozono' : 
+            return 'info Ozono';
+        case 'PM25' : 
+            return 'info PM25';
+        case 'SO2' : 
+            return 'info SO2';
+        case 'NO2' : 
+            return 'info NO2';
     }
-    //HORAS
-    let hours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
-    //NIVEL DE CONTAMINANTE
-    let contaminantesMes = [];
-    //for(j = 0; j < dates.length; j++){
-        let nivelesContaminanteDia = [];
-        for(i = 0; i < 23; i++){
-            nivelesContaminanteDia.push( niveles[i] );   //nivel contaminante promedio
-        }
-        //contaminantesMes.push(nivelesContaminanteDia);
-        //console.log(nivelesContaminanteDia);
-    //}
-    //MOSTRAR GRAFICA
-    console.log(nivelesContaminanteDia);
-    var contadorDia = 0;
-    /*var t = setInterval(()=>{
-        diaGrafica.innerHTML = "";
-        if(contadorDia > dates.length - 1){
-            clearInterval(t);
-            return;
-        }
-        diaGrafica.innerHTML = contadorDia+1;
-        console.log("dia en el que eesta : " + contadorDia);
-        console.log(contaminantesMes[contadorDia]);
-        */
-        var data = [
-            {
-                title: 'Niveles de ' + contaminante,
-                x: hours,
-                y: nivelesContaminanteDia,
-                type: 'scatter',
+}
+
+
+let imprimirGrafica = (contaminante,fechas,niveles,hora_max) =>{
+    
+    //HTML
+        //HORA
+        let horaMax = document.getElementById('hora-cont');
+        horaMax.innerHTML = "La hora con mayor nivel de contaminante es " + hora_max;
+        //INFO
+        let infoCont = document.getElementById('info-cont');
+        console.log(contaminante);
+        infoCont.innerHTML = info(contaminante);
+        //GRAFICA
+        GRAFICA = document.getElementById('contaminante-grafica');
+        document.getElementById( "contaminante-info" ).style.display="block";
+            //FECHAS
+            let dates = [];
+            var x = 0;
+            while(fechas[x]){
+                dates.push( fechas[x] );
+                x++;
             }
-        ];
-        Plotly.newPlot(GRAFICA, data);
-        /*contadorDia++;
-    },1000);*/
+            //HORAS
+            let hours = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
+            //NIVEL DE CONTAMINANTE
+            let nivelesContaminanteDia = [];
+            for(i = 0; i < 23; i++){
+                nivelesContaminanteDia.push( niveles[i] );   //nivel contaminante promedio
+            }
+            //MOSTRAR GRAFICA
+            console.log(nivelesContaminanteDia);
+            var contadorDia = 0;
+            var data = [
+                {
+                    title: 'Niveles de ' + contaminante,
+                    x: hours,
+                    y: nivelesContaminanteDia,
+                    type: 'scatter',
+                }
+            ];
+            Plotly.newPlot(GRAFICA, data);
 
 }
 
